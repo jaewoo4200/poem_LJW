462c7fdbe491efa64a2733d867f1e1e801449801
Jaewoo <doni04164@gmail.com>
2019-04-12 17:38:02 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jaewoo4200/poem_LJW",
+    python_requires = '>=3.7',
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.7",
